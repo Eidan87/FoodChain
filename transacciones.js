241470881697bd129c29020ca8d3ad45b4dcb01a
Eidan87 <42222419+Eidan87@users.noreply.github.com>
2023-03-18 04:36:16 +0100

    Create transacciones.js

---
+++ transacciones.js
@@ -0,0 +1,9 @@
+class Transaccion {
+  constructor(comprador, producto, cantidad, precio, fecha) {
+    this.comprador = comprador
+    this.producto = producto
+    this.cantidad = cantidad
+    this.precio = precio
+    this.fecha = fecha
+  }
+}
